548d8ef08e61fc1ece2df9ee23e54331f69c6b17
Apurv <aashah@umass.edu>
2021-02-20 00:56:12 -0500

    Added functionanilty to add kudo to our database

--- cs320.js
+++ cs320.js
@@ -8,13 +8,39 @@ const cors = require('cors');
 const app = express();
 app.use(cors());
 
+/*
+Kudo Schema:
+kudo : {
+	from : {type: String, explaination : "This is the user that is giving the kudo"},
+	to : {type: String, explaination : "This is the user that is recieving the kudo"},
+	kudo : {type : String,  explaination : "The actual text of the kudo"},
+	tags : {type: Array of strings, explaination : "The list of company value tags that the reciever displayed"},
+	emotes:{IDK yet how to do this}
+}
+
+Thought - This is a great schema as it includes everything that we thought of to be included in the kudos. 
+		  This can also be easily rendered into the feed when required by the frontend!
+
+Removed Kudos:
+kudo : {
+	removed_by : {type: String, explaination : "This is the admin user that is removing the kudo"},
+	from : {type: String, explaination : "This is the user that is giving the kudo"},
+	to : {type: String, explaination : "This is the user that is recieving the kudo"},
+	kudo : {type : String,  explaination : "The actual text of the kudo"},
+}
+
+*/
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
 // 	//correct password = rodriguezka
 // 	query = {email:"Kaitlin_Rodriguez@outbacktechnology.com", password:"rodriguezka"}
-	
+
+const uri = "mongodb+srv://user:cs320team1@cs320.t0mlm.mongodb.net/outback-tech?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -37,7 +63,6 @@ app.post('/api/verify', (req, res) => {
 		const employees = findItems();
 		const verify = async (employees,query) => {
 			found = false;
-
 			await employees.then(value  => {
 				value.forEach(function(element){
 				  if(element.email === query.email){
@@ -49,7 +74,28 @@ app.post('/api/verify', (req, res) => {
 
 		verify(employees,req.body)
 	});
-  // res.send(
-  //   `I received your POST request. This is what you sent me: ${req.body.post}`,
-  // );
 });
+
+app.post('/api/add_kudo', (req, res) => {
+	console.log(req.body);
+	const uri = "mongodb+srv://user:cs320team1@cs320.t0mlm.mongodb.net/outback-tech?retryWrites=true&w=majority";
+	const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+  	client.connect(err => {
+	assert.equal(err, null);
+	
+	const add_kudo = async (query) => {
+		const db = client.db("outback-tech");
+		const kudos = db.collection("Kudos");
+		kudos.insertOne(query, function(err, res) {
+		    if (err) throw err;
+		    console.log("1 document inserted");
+		});
+		res.send(`${true}`,);
+	}
+	
+	add_kudo(req.body)
+	});
+});
+
+
