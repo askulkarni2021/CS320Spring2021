0fe180703ccf03240eb82bbe821f5fb7946a6779
Apurv <aashah@umass.edu>
2021-02-22 14:23:36 -0500

    Added data using a script and developed a new endpoint

--- cs320.js
+++ cs320.js
@@ -11,8 +11,8 @@ app.use(cors());
 /*
 Kudo Schema:
 kudo : {
-	from : {type: String, explaination : "This is the user that is giving the kudo"},
-	to : {type: String, explaination : "This is the user that is recieving the kudo"},
+	from : {type: int, explaination : "This is the userID is giving the kudo"},
+	to : {type: int, explaination : "This is the userID that is recieving the kudo"},
 	kudo : {type : String,  explaination : "The actual text of the kudo"},
 	tags : {type: Array of strings, explaination : "The list of company value tags that the reciever displayed"},
 	emotes:{IDK yet how to do this}
@@ -44,7 +44,6 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
 app.post('/api/verify', (req, res) => {
 	console.log(req.body);
 	const uri = "mongodb+srv://user:cs320team1@cs320.t0mlm.mongodb.net/outback-tech?retryWrites=true&w=majority";
@@ -60,18 +59,20 @@ app.post('/api/verify', (req, res) => {
 			client.close();
 			return employees;
 		};
+
 		const employees = findItems();
+
 		const verify = async (employees,query) => {
 			found = false;
 			await employees.then(value  => {
 				value.forEach(function(element){
 				  if(element.email === query.email){
 				    found = element.password===query.password;}
+				    res.send({found:found, uid:element.employeeId});
 				})
 			});
-			res.send(`${found}`,);
+			res.send({found:found});
 		}
-
 		verify(employees,req.body)
 	});
 });
@@ -89,12 +90,57 @@ app.post('/api/add_kudo', (req, res) => {
 		const kudos = db.collection("Kudos");
 		kudos.insertOne(query, function(err, res) {
 		    if (err) throw err;
-		    console.log("1 document inserted");
 		});
 		res.send(`${true}`,);
 	}
 	
 	add_kudo(req.body)
+	});
+});
+
+app.post('/api/profile_incoming', (req, res) => {
+	console.log(req.body);
+	const uri = "mongodb+srv://user:cs320team1@cs320.t0mlm.mongodb.net/outback-tech?retryWrites=true&w=majority";
+	const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+  	client.connect(err => {
+	assert.equal(err, null);
+	const db = client.db("outback-tech");
+	const employeesCollection = db.collection("Employees Database");
+	const findEmployees = async () => { 
+		const employees = await employeesCollection.find({}).toArray();
+			//console.log(employees);
+		return employees;
+	};
+	const kudosCollection = db.collection("Kudos");	
+	const findKudos = async () => { 
+		const kudos = await kudosCollection.find({}).toArray();
+			//console.log(employees);
+		return kudos;
+	};
+	const employees = findEmployees();
+	const kudos = findKudos();
+
+	const find_incoming = async (employees,kudos,user) => { 
+		var employee;
+		await employees.then(value  => {
+			value.forEach(function(element){
+				if(element.email === user.email){
+				 	employee = element;
+				}
+			})
+		});
+		await kudos.then(value  => {
+			const incoming_kudos = [];
+			value.forEach(function(element){
+			//Need to work on this part, need help. Cant access the employee variable
+				
+			})
+			console.log(incoming_kudos);
+		});
+		
+	}
+	find_incoming(employees,kudos,req.body);
 	});
 });
 
