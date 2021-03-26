1d5667344bf6ef0049c01d50a1fa2b058d325807
Apurv <aashah@umass.edu>
2021-03-26 14:15:01 -0400

    verify_settings

--- cs320.js
+++ cs320.js
@@ -47,8 +47,8 @@ app.listen(port, () => console.log(`Listening on port ${port}`));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const findEmployees = async (employeesCollection) => { 
-	const employees = await employeesCollection.find({}).toArray();
+const findEmployees = async (employeesCollection, filter_query = {}) => { 
+	const employees = await employeesCollection.find(filter_query).toArray();
 	return employees;
 };
 const findKudos = async (kudosCollection,filter_query) => { 
@@ -308,26 +308,52 @@ app.post('/api/data/get_core_values', (req, res) => {
   });
 
 // Settings endpoints
-app.post('/api/data/get_core_values', (req, res) => {
-	console.log(req.body);
+app.post('/api/verify_settings', (req, res) => {
 	const companyName = req.body.uri;
 	const uri = "mongodb+srv://user:cs320team1@cs320.t0mlm.mongodb.net/" + companyName + "?retryWrites=true&w=majority";
 	const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-  	client.connect(err =>  {
+  	client.connect(err => {
 		assert.equal(err, null);
 		const db = client.db(companyName);
-		const Collection = db.collection("Employees Database");
+		const employeesCollection = db.collection("Employees Database");
+		const employees = findEmployees(employeesCollection, {employeeId:req.body.uid});
 
-		const employees = findEmployees(Collection);
-
-		const change_password = async (req, employees) => {
-			await val_em.then(value  => {
-			console.log(value[0].values);
+		const verify = async (employees,query) => {
+			found = false;
+			await employees.then(value  => {
+				//console.log(value.password);
+				found = (req.body.pass === value[0].password)
+				console.log(value);
+				console.log(found);
+			});
 			client.close();
-			res.send(value[0].values);
-		});
-		};
-		send_data(val_em);
+			res.send({found:found});
+		}
+		verify(employees,req.body)
 	});
-  });
+});
+
+// app.post('/api/data/change_password', (req, res) => {
+// 	console.log(req.body);
+// 	const companyName = req.body.uri;
+// 	const uri = "mongodb+srv://user:cs320team1@cs320.t0mlm.mongodb.net/" + companyName + "?retryWrites=true&w=majority";
+// 	const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+//   	client.connect(err =>  {
+// 		assert.equal(err, null);
+// 		const db = client.db(companyName);
+// 		const Collection = db.collection("Employees Database");
+
+// 		const employees = findEmployees(Collection);
+
+// 		const change_password = async (req, employees) => {
+// 			await val_em.then(value  => {
+// 			console.log(value[0].values);
+// 			client.close();
+// 			res.send(value[0].values);
+// 		});
+// 		};
+// 		send_data(val_em);
+// 	});
+//   });
