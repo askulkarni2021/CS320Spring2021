db0972ddc342b6c376b9ea15533e0cb1f420a403
tq-o <onquocthai1106@gmail.com>
2021-04-04 01:35:29 -0400

    update logo in dark state

--- front-end/src/pages/Login.js
+++ front-end/src/pages/Login.js
@@ -3,13 +3,14 @@ import { Button, Grid, TextField, Typography, FormControl, InputLabel,
 OutlinedInput, InputAdornment, IconButton } from '@material-ui/core'
 import { Visibility, VisibilityOff } from '@material-ui/icons'
 import logo from '../kudos_logo.png'
+import wlogo from '../kudos_logo_white.png'
 
 export default function Login(props) {
   const [user, setUser] = useState('');
   const [pass, setPass] = useState('');
   const [showPass, setShowPass] = useState(false);
   const [invalidLogin, setInvalidLogin] = useState(false);
-
+  
   // updates either user state or pass state when typed
   const handleChange = (prop) => (event) => {
     if (prop === 'user') {
@@ -63,7 +64,7 @@ export default function Login(props) {
   return (
     <Grid container direction="column" justify="center" alignItems="center" style={{ minHeight: '100vh'}}>
       <Grid item xs={12} sm={6} md={3} style={{padding: '10px', width: '80%'}}>
-          <img className="img-responsive" src={logo} alt="logo"/>
+          {localStorage.getItem('darkState') === "false"? <img className="img-responsive" src={logo} alt="logo"/>: <img className="img-responsive" src={wlogo} alt="wlogo"/>}
           <form onSubmit={(e) => {e.preventDefault(); handleSubmit();}} style={{display: 'flex', flexDirection: 'column'}}>
             <TextField label="username"
               error={invalidLogin}
